bbffc92813e1aebeaf2b1702ed9dd07b886be116
mat557 <71515771+mat557@users.noreply.github.com>
2024-02-17 02:20:30 +0600

    allowed origin changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://hibernet-edu.netlify.app'
+    'https://65cfc0a9d7d42a1b49ae7360--hibernet-edu.netlify.app/login'
 ]
 
 module.exports = allowedOrigins
