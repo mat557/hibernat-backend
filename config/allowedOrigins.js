6e731851552ce0cc3d5ae3e20561055d9b57d06a
mat557 <71515771+mat557@users.noreply.github.com>
2024-02-17 19:56:56 +0600

    Linked added to allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
+    'https://65cfc0a9d7d42a1b49ae7360--hibernet-edu.netlify.app/login',
+    'https://hibernet-edu.netlify.app',
     'http://localhost:3000',
-    'https://65cfc0a9d7d42a1b49ae7360--hibernet-edu.netlify.app/login'
 ]
 
 module.exports = allowedOrigins
